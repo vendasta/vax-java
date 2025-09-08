4a120e229aa3a9fab07c7cda34d142eeeccf7834
Leo Melnyk <lmelnyk@vendasta.com>
2025-09-08 14:14:49 -0600

    fix(Java SDKGen) - Retain backwards compatibilty for doRequest
    [MCR-211]

--- src/main/java/com/vendasta/vax/GRPCClient.java
+++ src/main/java/com/vendasta/vax/GRPCClient.java
@@ -1,6 +1,7 @@
 package com.vendasta.vax;
 
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
@@ -57,6 +58,47 @@ public abstract class GRPCClient<T extends io.grpc.stub.AbstractStub<T>> extends
     protected abstract T newBlockingStub(ManagedChannel channel);
 
 
+    /**
+     * @deprecated This method is deprecated. Use {@link #doRequest(Function, RequestOptions.Builder)} instead.
+     */
+    @Deprecated
+    @SuppressWarnings("unchecked")
+    protected <V> V doRequest(String func, com.google.protobuf.AbstractMessage request, RequestOptions.Builder builder) throws SDKException {
+        RequestOptions options = this.buildVAXOptions(builder);
+        T stub;
+        if (options.getTimeout() > 0) {
+            stub = blockingStub.withDeadlineAfter((long) (options.getTimeout() * 1000), TimeUnit.MICROSECONDS);
+        } else {
+            stub = blockingStub.withDeadlineAfter(1, TimeUnit.DAYS);
+        }
+
+        if (options.getIncludeToken()) {
+            stub = stub.withCallCredentials(credentialsManager);
+        } else {
+            stub = stub.withCallCredentials(null);
+        }
+
+        try {
+            Object resp = stub.getClass().getMethod(func, request.getClass()).invoke(stub, request);
+            return (V) resp;
+        } catch (InvocationTargetException e) {
+            throw new SDKException(e.getTargetException().getMessage());
+        } catch (Exception e) {
+            throw new SDKException(e.getMessage());
+        }
+    }
+
+    /**
+     * Executes a gRPC request using a type-safe function approach.
+     * This is the recommended method for making gRPC calls as it provides compile-time type safety
+     * and better performance by avoiding reflection.
+     * 
+     * @param <V> the return type of the method call
+     * @param methodCall the function that defines the gRPC method to call
+     * @param builder the request options builder
+     * @return the result of the method call
+     * @throws SDKException if there's an error during the request
+     */
     protected <V> V doRequest(Function<T, V> methodCall, RequestOptions.Builder builder) throws SDKException {
         RequestOptions options = this.buildVAXOptions(builder);
         T stub;
